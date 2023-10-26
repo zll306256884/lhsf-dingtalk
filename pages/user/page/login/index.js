b54396d47d2d7b155565a9ac69aa4a7155cecb6b
pengwenjuan <13253386045@163.com>
2023-10-26 18:07:30 +0800

    feat:工作台

--- pages/user/page/login/index.js
+++ pages/user/page/login/index.js
@@ -2,7 +2,6 @@ import { isEqual, isEmptyArray } from "../../../../utils/utils"
 import ddUtils from "../../../../utils/ddUtils"
 import userServer from "../../../../server/userServer"
 import request from "../../../../utils/request"
-import loginImage from "../../../../assets/images/login.png"
 
 
 const app = getApp();
@@ -12,7 +11,6 @@ Page({
         navbarData: {
             title: "授权登录",
         },
-        src:loginImage
     },
 
     onLoad() {
