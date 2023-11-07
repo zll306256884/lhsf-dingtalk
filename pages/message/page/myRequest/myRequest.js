1327ad80ccbe748c53db8b2365e0a24ba2f0a619
rxj <15062270410@163.com>
2023-11-07 14:30:34 +0800

    feat:我的请求

---
+++ pages/message/page/myRequest/myRequest.js
@@ -0,0 +1,15 @@
+Page({
+  data: {
+    navbarData: {
+      title: '我的请求'
+    },
+    tabList: [{
+        name: '我是默认值1'
+      },
+      {
+        name: '我是默认值2'
+      },
+    ],
+  },
+  onLoad() {},
+});
