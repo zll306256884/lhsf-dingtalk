842814fdda22b688d5056a78f2fb0725d7202f9e
pengwenjuan <13253386045@163.com>
2023-11-08 14:01:21 +0800

    feat:筛选器样式调整

--- pages/databoard/index.js
+++ pages/databoard/index.js
@@ -1,83 +1,83 @@
-
-import ddUtils from '../../utils/ddUtils'
+import ddUtils from "../../utils/ddUtils";
 
 Page({
   data: {
-    visibel:false,
-    navbarData:{
+    visibel: false,
+    navbarData: {
       title: "临海市项目工程数据看板"
     },
-    tabs: [{
-      title: "项目概览",
-    }, {
-      title: "年度资金管控",
-    }, {
-      title: "年度招标进度",
-    }],
-    currentTabIndex:0,
-    options:[
+    tabs: [
       {
-        label:"类型",
-        prop:"type",
-        value:[],
-        type:'select',
+        title: "项目概览"
+      },
+      {
+        title: "年度资金管控"
+      },
+      {
+        title: "年度招标进度"
+      }
+    ],
+    currentTabIndex: 0,
+    options: [
+      {
+        label: "类型",
+        prop: "type",
+        value: [],
+        type: "select",
         option: [
           {
-              id:"1",
-              label: '12',
-              selected: false,
+            id: "1",
+            label: "进度看板高考分数",
+            selected: false
           },
           {
-              id:"2",
-              label: '34',
-              selected: false,
+            id: "2",
+            label: "进度看板高考分数",
+            selected: false
           },
           {
-              id:"3",
-              label: '56',
-              selected: false,
-          },
-      ],
+            id: "3",
+            label: "进度看板高考分数",
+            selected: false
+          }
+        ]
       },
       {
-        label:"人员",
-        value:"",
-        prop:"userName",
-        type:'input' 
-      },
-    ],
+        label: "人员",
+        value: "",
+        prop: "userName",
+        type: "input"
+      }
+    ]
   },
-  dialogScreenDateRef:null,
-  onLoad() {
-  },
-  onTabChange(e){
+  dialogScreenDateRef: null,
+  onLoad() {},
+  onTabChange(e) {
     this.setData({
-      currentTabIndex:e
-    })
-
-  },
-  _onSaveDialogScreenDateRef:function (ref) {
-    this.dialogScreenDateRef = ref;
-  },
-  tapName(e){
-    // this.setData({
-    //   visibel:true
-    // })
-    ddUtils.navigateTo({
-      url: `/pages/databoard/page/projectInfo/index`
+      currentTabIndex: e
     });
   },
-  _bindScreenDateCallBack(data){
-    console.log(data);
-
+  _onSaveDialogScreenDateRef: function(ref) {
+    this.dialogScreenDateRef = ref;
   },
-  onDialog(data){
+  tapName(e) {
     this.setData({
-      visibel:data
-    })
+      visibel: true
+    });
+    // ddUtils.navigateTo({
+    //   url: `/pages/databoard/page/projectInfo/index`
+    // });
   },
-  onBindSureTap(data){
+  _bindScreenDateCallBack(data) {
     console.log(data);
-    this.onDialog(false)
+  },
+  onDialog(data) {
+    this.setData({
+      visibel: data
+    });
+  },
+  onBindSureTap(data) {
+    console.log(data);
+    this.onDialog(false);
   }
 });
