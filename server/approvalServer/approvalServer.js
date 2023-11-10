18d65cdf2052b7d9592b990c6a4bf2151dd13177
wuxuan <wuxuanxzmm@163.com>
2023-11-10 11:24:01 +0800

    审批记录

--- server/approvalServer/approvalServer.js
+++ server/approvalServer/approvalServer.js
@@ -3,21 +3,10 @@ import {
 } from "../../utils/config.js"
 
 module.exports = {
-   //查询列表
-   API_SELECT_LIST: API_HOST + "/jflow/audit/selectAuditPending",
-  
-  // //登录
-  // API_LOGIN: API_HOST + "/platform/account/app/appLogin",
-  // //获取用户信息
-  // API_GET_USER_INFO: API_HOST + "/platform/user/getUserInfoByToken",
-  // //项目列表
-  // API_PROJECT_LIST: API_HOST + "/platform/projectBindingAccount/queryMyProjectList",
-  // //  基本信息
-  // API_BASE_INFO: API_HOST + "/platform/user/selectUserDetailById",
-  // //  在职项目
-  // API_IN_PROJECT: API_HOST + "/integratedService/peopleProject/queryServeProject",
-  // // 获取验证码图片 
-  // API_VERIFY_CODE: API_HOST + "/common/createVerifyCode",
-  // // 修改密码
-  // API_EDIE_CODE: API_HOST + "/platform/account/modifyPassword",
+  //查询列表
+  API_SELECT_LIST: API_HOST + "/jflow/audit/selectAuditPending",
+  // 获取审批节点
+  API_APPROVAL_LIST: API_HOST + "/jflow/jflowAuditRecord/selAllByKeyId",
+  // 获取下一个审批节点
+  API_NEXT_APPROVAL_NODE: API_HOST + "/jflow/jflowAuditRecord/selectFlowInfoByKeyId",
 }
