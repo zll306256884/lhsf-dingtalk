71b8e0900b24c0b69d3a7278d1319a360dfc402d
pengwenjuan <13253386045@163.com>
2023-11-22 17:16:29 +0800

    fix:全局返回按钮

--- pages/components/nav-bar/nav-bar.js
+++ pages/components/nav-bar/nav-bar.js
@@ -2,42 +2,24 @@ import ddUtils from "../../../utils/ddUtils";
 const app = getApp();
 
 Component({
-    mixins: [], // mixins 方便复用代码
-    /**
-     * 组件的属性列表
-     */
+    mixins: [], 
     props: {
       navbarData: {
-        title: "小程序",
-        titleColor: "#333333",
-        navBackgroundColor: "white",
-        showNavbarBack:true,
-        navBackRes: "/assets/images/arrow-left.png",
-        noBackgroundColor: false,
-        titleLeft: false,
-        noPlaceholder: false, //导航栏不占顶部位置
-        isCatchtap: false, //catch tap by navgationbar back
-        backWidth: 8,
-        backHeight: 16
-    },
+      },
         onNavBack: function () { }
     },
-
-    /**
-     * 组件的初始数据
-     */
     data: {
       navbarData: {
         title: "小程序",
+        showNavbarBack:true,
         titleColor: "#333333",
         navBackgroundColor: "white",
-        showNavbarBack:true,
         navBackRes: "/assets/images/arrow-left.png",
         noBackgroundColor: false,
         titleLeft: false,
         noPlaceholder: false, //导航栏不占顶部位置
         isCatchtap: false, //catch tap by navgationbar back
-        backWidth: 8,
+        backWidth: 16,
         backHeight: 16
     },
         _isNavClick: false, //防止返回按钮快速点击
@@ -45,11 +27,8 @@ Component({
 
     //组件创建时触发
     onInit() {
-      this.data.navbarData.showNavbarBack= this.props.navbarData.showNavbarBack
-      this.data.navbarData.navBackRes= this.props.navbarData.navBackRes
-      this.data.navbarData.title= this.props.navbarData.title
       this.setData({
-        navbarData:this.data.navbarData
+        navbarData:{...this.data.navbarData,...this.props.navbarData}
       })
     },
 
@@ -92,11 +71,9 @@ Component({
     methods: {
         _bindNavBack: function (e) {
             if (this.data._isNavClick) return;
-
             this.setData({
                 _isNavClick: true
             });
-
             if (this.props.navbarData && this.props.navbarData.isCatchtap) {
                 //触发返回回调
                 this.onNavBack();
