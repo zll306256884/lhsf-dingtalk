6371c6257713c6b0c51d47bb017b138fb46ce9ef
rxj <15062270410@163.com>
2023-11-29 16:06:25 +0800

    feat: 招标上传文件

--- pages/components/upload-file/upload-file.js
+++ pages/components/upload-file/upload-file.js
@@ -11,7 +11,7 @@ Page({
     console.log(option);
     if(option.type){
       this.setData({
-        webViewUrl: 'http://192.168.6.41/#/share/viewFile?type='+ option.type
+        webViewUrl: 'http://192.168.8.51:8080/#/share/viewFile?type='+ option.type
       })
     }
     this.webViewContext = dd.createWebViewContext('web-view-1')
@@ -21,12 +21,15 @@ Page({
     console.log('接受消息',e.detail)
     let pages = getCurrentPages()
     let prevPage = pages[pages.length - 2]
+    console.log('prevPage',prevPage)
     prevPage.uploadImageList._setImageList(e.detail.imgList)
     if(e.detail.type === '1'){
       prevPage.uploadTenderImageList._setImageList(e.detail.imgList)
     }else if(e.detail.type === '2'){
       prevPage.uploadOtherImgList._setImageList(e.detail.imgList)
     }
+    
+    prevPage.uploadImgRefList._setImageList(e.detail.imgList)
     dd.navigateBack()
   },
 });
