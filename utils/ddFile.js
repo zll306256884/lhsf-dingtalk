9e3cf697def5e3601e9d6a38d52f4017461bbcf2
p <784312297@qq.com>
2023-12-09 00:06:48 +0800

    fix: 接口调用有lodin不需要加节流

--- utils/ddFile.js
+++ utils/ddFile.js
@@ -2,42 +2,32 @@ import config from "./config"
 import request from "./request"
 import ddUtils from "./ddUtils"
 
-function downloadFile(url) {
-    // let fileName
-    let debounce = null;
-    return function(){
-      if(debounce){
-        clearTimeout(debounce);
-      }
-      debounce  = setTimeout(()=> {
-        request.doPostRequest({
-          url: config.API_FILE_SETURL,
-          data:{
+function downloadFile (url) {
+    request.doPostRequest({
+        url: config.API_FILE_SETURL,
+        data: {
             fileName: url,
-          },
-          success: result => {
-            if(result.code == 1000) {
-            //  fileName = result.data.split('/')
-              // 获取钉盘文件信息
-              request.doPostRequest({
-                url: config.API_FILE_GETURL,
-                data: {
-                  targetPath: result.data,
-                },
-                success: res => {
-                let ddDownFileParams =  ddUtils.urlParams(res.data)
+        },
+        success: result => {
+            if (result.code == 1000) {
+                //  fileName = result.data.split('/')
                 // 获取钉盘文件信息
-                ddUtils.ddDownFile(ddDownFileParams)
-                debounce = null;
-                }
-              })
+                request.doPostRequest({
+                    url: config.API_FILE_GETURL,
+                    data: {
+                        targetPath: result.data,
+                    },
+                    success: res => {
+                        let ddDownFileParams = ddUtils.urlParams(res.data)
+                        // 获取钉盘文件信息
+                        ddUtils.ddDownFile(ddDownFileParams)
+                        debounce = null;
+                    }
+                })
             }
-          }
-        })
-      ,10000})
-    }
-  }
-  module.exports={
+        }
+    })
+}
+module.exports = {
     downloadFile,
-
-  }
+}
