922e95a64e58e478e350072939b4d698bde96c2f
chen1918 <547983493@qq.com>
2023-12-16 14:49:37 +0800

    修复缺陷小程序-支付申请-已撤回审批消息-不是创建人不能有编辑权限

--- pages/work/page/addPaymentDetail/addPaymentDetail.js
+++ pages/work/page/addPaymentDetail/addPaymentDetail.js
@@ -121,11 +121,11 @@ Page({
           infoData: res.data
         })
         let dingTalkFormList = [
-          { '事项类型': '款项支付' },
-        { '所属项目': res.data.projectName },
-        { '合同名称': res.data.contractName },
-        { '支付金额': res.data.payAmount + '万元' },
-        { '收款单位': res.data.receiverUnit }
+          { '事项类型：': '款项支付' },
+        { '所属项目：': res.data.projectName },
+        { '合同名称：': res.data.contractName },
+        { '支付金额：': res.data.payAmount + '万元' },
+        { '收款单位：': res.data.receiverUnit }
         ]
         this.setData({
           dingTalkFormList
