5083570c2a96dbacbddb179b13e93c24fc476174
wuxuan <wuxuanxzmm@163.com>
2023-12-18 17:11:34 +0800

    小程序接海康

--- utils/config.js
+++ utils/config.js
@@ -1,13 +1,13 @@
 
 //  测试配置
-// const BASE_API_HOST = "http://192.168.6.41";
-// const API_HOST = BASE_API_HOST + "/prod-api";
+const BASE_API_HOST = "http://192.168.6.41";
+const API_HOST = BASE_API_HOST + "/prod-api";
 
 // const BASE_API_HOST = "http://192.168.8.81";
 
 // 线上配置
-const BASE_API_HOST = "https://xmgk.lhbigdata.com";
-const API_HOST = BASE_API_HOST + "/prod-api";
+// const BASE_API_HOST = "https://xmgk.lhbigdata.com";
+// const API_HOST = BASE_API_HOST + "/prod-api";
 
 
 const API_IMG_URL = "https://eos-shanghai-2.cmecloud.cn/";
