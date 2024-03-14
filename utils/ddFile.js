c77e7e0dc56f1cdcf79922311733c6b97715dce6
pq <M78aotman@gmail.com>
2024-03-14 19:02:15 +0800

    fix：文件预览优化

--- utils/ddFile.js
+++ utils/ddFile.js
@@ -3,46 +3,46 @@ import request from "./request"
 import ddUtils from "./ddUtils"
 
 function downloadFile (url) {
-  // if(url.indexOf('https://') == -1) {
-  //   url = 'https://'+url
-  // }
-  //   request.doPostRequest({
-  //       url: config.API_FILE_SETURL,
-  //       data: {
-  //           fileName: url,
-  //       },
-  //       success: result => {
-  //           if (result.code == 1000) {
-  //               //  fileName = result.data.split('/')
-  //               // 获取钉盘文件信息
-  //               request.doPostRequest({
-  //                   url: config.API_FILE_GETURL,
-  //                   data: {
-  //                       targetPath: result.data,
-  //                   },
-  //                   success: res => {
-  //                       let ddDownFileParams = ddUtils.urlParams(res.data)
-  //                       // 获取钉盘文件信息
-  //                       ddUtils.ddDownFile(ddDownFileParams)
-  //                       debounce = null;
-  //                   }
-  //               })
-  //           }
-  //       }
-  //   })
-  
-  request.doPostRequest({
-    url: config.API_FILE_GETURL,
-    data: {
-        targetPath: url,
-    },
-    success: res => {
-      let ddDownFileParams = ddUtils.urlParams(res.data)
-      // 获取钉盘文件信息
-      ddUtils.ddDownFile(ddDownFileParams)
-      // debounce = null;
-    }
-})
+  // 文件优化以免漏网之鱼，正常走上面
+  if(url.indexOf('https://eos-shanghai-2') == -1) {
+    request.doPostRequest({
+      url: config.API_FILE_GETURL,
+      data: {
+          targetPath: url,
+      },
+        success: res => {
+          let ddDownFileParams = ddUtils.urlParams(res.data)
+          // 获取钉盘文件信息
+          ddUtils.ddDownFile(ddDownFileParams)
+          // debounce = null;
+        }
+    })
+  }else{
+    request.doPostRequest({
+        url: config.API_FILE_SETURL,
+        data: {
+            fileName: url,
+        },
+        success: result => {
+            if (result.code == 1000) {
+                //  fileName = result.data.split('/')
+                // 获取钉盘文件信息
+                request.doPostRequest({
+                    url: config.API_FILE_GETURL,
+                    data: {
+                        targetPath: result.data,
+                    },
+                    success: res => {
+                        let ddDownFileParams = ddUtils.urlParams(res.data)
+                        // 获取钉盘文件信息
+                        ddUtils.ddDownFile(ddDownFileParams)
+                        debounce = null;
+                    }
+                })
+            }
+        }
+    })
+  }
 }
 module.exports = {
     downloadFile,
