756e1b039f06a542e6a3054e5eb96ad0c5851a31
zenglingling <306256884@qq.com>
2024-10-16 20:50:42 +0800

    fix:项目进度

--- pages/databoard/page/gantt/gantt.js
+++ pages/databoard/page/gantt/gantt.js
@@ -2,12 +2,14 @@ const app = getApp();
 import config from "../../../../utils/config";
 Page({
   data: {
-    webViewUrl: config.BASE_API_HOST + '/#/share/gantt'
+    webViewUrl: 'http://192.168.44.179:8080/#/share/gantt'
+    // webViewUrl: config.BASE_API_HOST + '/#/share/gantt'
   },
   onLoad(option) {
     console.log('optiongantt', option);
     this.setData({
-      webViewUrl: `${config.BASE_API_HOST}/#/share/gantt?projectId=${option.projectId}`
+      webViewUrl: `http://192.168.44.179:8080/#/share/gantt?projectId=${option.projectId}`
+      // webViewUrl: `${config.BASE_API_HOST}/#/share/gantt?projectId=${option.projectId}`
     }) //http://192.168.6.41/#/share/gantt?projectId=12019020001
     this.webViewContext = dd.createWebViewContext('web-view-1')
     this.webViewContext.postMessage({ tokenStr: app.globalData.userInfo.userToken })
