8a62c41bedfb4de7baebc1dc8991967a0adc431a
yuyangyang <3750621763@qq.com>
2024-11-01 18:21:31 +0800

    fix：合同

--- pages/work/page/contractApprovalCreatAndEdit/contractApprovalCreatAndEdit.js
+++ pages/work/page/contractApprovalCreatAndEdit/contractApprovalCreatAndEdit.js
@@ -353,6 +353,16 @@ Page({
     //   });
     //   return
     // }
+    if(this.form.getFieldValue('projectType') && this.data.projectLeaderListOptions.length) {
+      const index = this.data.projectLeaderListOptions.findIndex(e => e.personId === this.data.userId)
+      if(this.form.getFieldValue('projectType') === '1' && index === -1) {
+        ddUtils.showToast({
+          title: '注意：仅项目负责人可发起流程',
+          duration: 2000
+        });
+        return
+      }
+    }
     let contractName = this.form.getFieldValue('contractName') || ''
     if( data.proType === 0){
       this.form.setFieldValue('title', data.name+'-'+contractName)
